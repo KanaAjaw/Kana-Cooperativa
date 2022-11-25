14d54f70f4768dab52ea33d688bc44d3e4a72162
KanaAjaw <ajaw.cooperativa@gmail.com>
2022-11-25 11:59:37 -0600

    Se arregla el scroll en la página de Solicitud cuando se muestra por primera vez el identificador

--- landing_page/Scripts/Herramientas.js
+++ landing_page/Scripts/Herramientas.js
@@ -15,7 +15,7 @@ export function redirigir_con_idu(pagina_landing_page, id_unico){
         window.location.replace(`https://kanacooperativa.com.mx/landing_page/${pagina_landing_page}`)
 }
 
-export function scroll_hacia(elemento,padding_top){
+export function scroll_hacia(elemento,padding_top=0){
     let posicion = elemento.offsetTop-padding_top;
     window.scrollTo({ top: posicion, behavior: 'smooth'});
 }
