916ef709d14d8c303a4e0df1c253952574d34911
KanaAjaw <ajaw.cooperativa@gmail.com>
2023-03-01 02:24:54 -0600

    Actualización de la página principal. La nueva página tan solo incluye un Hero y queda pendiente agregar la información de las áreas

---
+++ Estilos/tailwindcss.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#2c78bcff',
+          dark: '#333333ff',
+          highlight: '#ffcc00ff',
+        },
+        fontFamily: {
+            title: ['Inter','Poppins','sans-serif'],
+            poppins: ['Poppins','"Fira Sans"','sans-serif']
+        }
+      }
+    }
+  }
